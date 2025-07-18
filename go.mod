cf6bc4c969ac69930861dc0af1a2cda317ab23ce
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-07-18 18:48:29 +0200

    able to import files, spews out all entries rn

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module find-hijackable-dns
 
 go 1.24
+
+require github.com/bwesterb/go-zonefile v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/bwesterb/go-zonefile"
+	"os"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	zonefileFilename := flag.String("zonefile", "gov.txt", "file name of the zonefile you want to analyze")
+	flag.Parse()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	file, err := os.ReadFile(*zonefileFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open Zonefile: %s", *zonefileFilename))
 	}
+	zf, err := zonefile.Load(file)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to load Zonefile: %s", *zonefileFilename))
+	}
+	for _, entrie := range zf.Entries() {
+		fmt.Printf("%s\n", entrie.Type())
+	}
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
